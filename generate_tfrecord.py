4ec4d8a8ee568defc49fb21be9b38851087682fa
RonNguyen77 <96341237+RonNguyen77@users.noreply.github.com>
2022-01-19 09:02:40 +0700

    Update generate_tfrecord.py

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -13,7 +13,8 @@ from __future__ import absolute_import
 import os
 import io
 import pandas as pd
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
+import tensorflow_datasets as tfds
 
 from PIL import Image
 from object_detection.utils import dataset_util
